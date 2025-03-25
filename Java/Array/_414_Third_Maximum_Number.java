53b1d893a11b2d2067ea2593bba4cdaa31408899
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2025-03-25 23:14:22 +0700

    update 414

--- Java/Array/_414_Third_Maximum_Number.java
+++ Java/Array/_414_Third_Maximum_Number.java
@@ -28,3 +28,38 @@ public class _414_Third_Maximum_Number {
         System.out.println("Result: " + b);
     }
 }
+
+// Cách này lâu điên
+// public static int thirdMax(int[] nums) {
+// long m3 = Long.MIN_VALUE;
+// long m2 = Long.MIN_VALUE;
+// long m1 = Long.MIN_VALUE;
+// int n = nums.length;
+
+// for (int i = 0; i < nums.length - 1; i++) {
+// int minIndex = i;
+// for (int j = i + 1; j < nums.length; j++) {
+// if (nums[j] > nums[minIndex]) {
+// minIndex = j;
+// }
+// }
+// int temp = nums[i];
+// nums[i] = nums[minIndex];
+// nums[minIndex] = temp;
+// }
+
+// System.out.println("nums: " + java.util.Arrays.toString(nums));
+
+// for (int i = 0; i < n; i++) {
+// m1 = Math.max(m1, nums[i]);
+// if (nums[i] != m1) {
+// m2 = Math.max(m2, nums[i]);
+// if (nums[i] != m2) {
+// m3 = Math.max(m3, nums[i]);
+// }
+// }
+// }
+
+// return m3 == Long.MIN_VALUE ? (int) m1 : (int) m3;
+
+// }
